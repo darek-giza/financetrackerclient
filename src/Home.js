ff7cff97662dd91f0dfd86eaab86fc65e017ccb3
darek-giza <darek_giza@op.pl>
2020-02-07 23:00:02 +0100

    I changed the home page view

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import HomeBar from './Components/HomeBar'
-import StickyFooter from './StickyFooter'
+import { withStyles } from '@material-ui/styles';
+import Link from '@material-ui/core/Link';
 
+const styles = {
+  intro:{
+    width: '100%',
+    height:'900px',
+    backgroundImage: 'url(' +require('./Images/intro_image.jpg')+')',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    color:'lightblue',
+    letterSpacing:'20px',
+    fontSize:'90px',
+    textAlign:'right',
+  },
+  button:{
+    fontSize:'50px',
+    color:'inherit',
+    textTransform:'lowercase',
+  },
+  description:{
+    fontSize:'10px',
+    textAlign:'left',
+    letterSpacing:'2px',
+    fontSize:'24px',
+    marginBottom:'100px',
+    padding:'480px 0px 0px 70px '
+
+  }
+};
 
 class Home extends Component {
     render(){
+      const {classes} = this.props;
     return (
-        <div>
-
-            <HomeBar/>
-            
-            <h1>Hello on my home page ... </h1>
-            
-            <StickyFooter/>
-        
-        </div> 
+      <div className={classes.intro}>
+            <span>Finance tracker</span><br/>
+            <a><Link href="/signin" className={classes.button} color="primary">Log in here ... </Link></a>
+                  <br/>
+            <div className={classes.description}>
+                      Want to know where your money is?<br/>
+                      Log in, add your income and expenses and <br/>
+                      check what's going on your money.<br/>
+                      You will find that a good life costs a lot ...<br/>
+                      Will you see if you can plan any extra expenses for the next month?<br/>
+                      I hope you have enough money for your next payment.     
+            </div>
+      </div>
     )
+    }
   }
-}
-  export default Home;
+
+  export default withStyles(styles)(Home);
 
