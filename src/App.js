b68cac5db4f21e031a707e7a72e649d64d0999b7
darek-giza <darek_giza@op.pl>
2020-03-21 14:02:07 +0100

    Change class App,Home to function componet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import User from './User';
@@ -11,23 +11,18 @@ import Logout from './Logout';
 import Signup from './Signup';
 
 
-class App extends Component {
-  state = {};
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/" exact={true} component={Home}></Route>
-          <Route path="/user" exact={true} component={User}></Route>
-          <Route path="/incomes" exact={true} component={Incomes}></Route>
-          <Route path="/expenses" exact={true} component={Expenses}></Route>
-          <Route path="/type" exact={true} component={Types}></Route>
-          <Route path="/signin" exact={true} component={SignIn}></Route>
-          <Route path="/signup" exact={true} component={Signup}></Route>
-          <Route path="/logout" exact={true} component={Logout}></Route>
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/user" exact={true} component={User}></Route>
+      <Route path="/incomes" exact={true} component={Incomes}></Route>
+      <Route path="/expenses" exact={true} component={Expenses}></Route>
+      <Route path="/type" exact={true} component={Types}></Route>
+      <Route path="/signin" exact={true} component={SignIn}></Route>
+      <Route path="/signup" exact={true} component={Signup}></Route>
+      <Route path="/logout" exact={true} component={Logout}></Route>
+    </Switch>
+  </Router>
+);
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import Link from '@material-ui/core/Link';
 import lightblue from '@material-ui/core/colors/lightBlue';
@@ -30,24 +30,18 @@ const styles = {
   },
 };
 
-class Home extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.intro}>
-        <span>Finance tracker</span>
-        <br />
-        <Link href="/signin" className={classes.link} color="primary">
-          Log in here ..
-        </Link>
-        <div className={classes.description}>
-          <p>Want to know where your money is?</p>
-          <p>Log in, add your income and expenses and</p>
-          <p>check what's going on your money ...?</p>
-        </div>
-      </div>
-    );
-  }
-}
-
+const Home = ({classes}) => (
+  <div className={classes.intro}>
+    <span>Finance tracker</span>
+    <br/>
+    <Link href="/signin" className={classes.link} color="primary">
+      Log in here ..
+    </Link>
+    <div className={classes.description}>
+      <p>Want to know where your money is?</p>
+      <p>Log in, add your income and expenses and</p>
+      <p>check what's going on your money ...?</p>
+    </div>
+  </div>
+);
 export default withStyles(styles)(Home);
