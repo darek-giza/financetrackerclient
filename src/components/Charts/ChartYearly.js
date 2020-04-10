488a6d3953e93240216db4ba645e0401e381d3a0
darek-giza <darek_giza@op.pl>
2020-04-10 17:57:37 +0200

    Fix yearly chart view

--- src/components/Charts/ChartYearly.js
+++ src/components/Charts/ChartYearly.js
@@ -1,20 +1,30 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { request } from "../../utils/request";
 import { Container } from '@material-ui/core';
 import './Chart.css';
 import FetchChart from "./FetchChart";
+import Typography from "@material-ui/core/Typography";
 
-const ChartYearly = ({shouldRefresh, onRefresh}) => {
+const ChartYearly = () => {
+  const [refresh,setRefresh] = useState(true);
+
   const load = useCallback(async () => {
     return await request('/api/chartYearly');
   }, []);
 
+  const onRefresh = useCallback(()=>{
+    setRefresh(false);
+  },[]);
+
   return (
     <Container>
+      <Typography variant="h2" component="h1" className="header">
+        Chart for 2020
+      </Typography>
       <FetchChart loadData={load}
-                  width={1000}
+                  width={1100}
                   height={300}
-                  shouldRefresh={shouldRefresh}
+                  shouldRefresh={refresh}
                   onRefresh={onRefresh}
       />
     </Container>
