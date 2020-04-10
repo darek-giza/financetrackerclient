6a3a3b8e55a1ece84954395a63f5189ccc0b0818
darek-giza <darek_giza@op.pl>
2020-04-10 16:40:16 +0200

    Nothing

--- src/components/Charts/ChartYearly.js
+++ src/components/Charts/ChartYearly.js
@@ -1,10 +1,10 @@
 import React, { useCallback } from 'react';
-import { request } from '../../utils/request';
+import { request } from "../../utils/request";
 import { Container } from '@material-ui/core';
 import './Chart.css';
-import FetchChart from './FetchChart';
+import FetchChart from "./FetchChart";
 
-const ChartYearly = () => {
+const ChartYearly = ({shouldRefresh, onRefresh}) => {
   const load = useCallback(async () => {
     return await request('/api/chartYearly');
   }, []);
@@ -14,6 +14,8 @@ const ChartYearly = () => {
       <FetchChart loadData={load}
                   width={1000}
                   height={300}
+                  shouldRefresh={shouldRefresh}
+                  onRefresh={onRefresh}
       />
     </Container>
   );
