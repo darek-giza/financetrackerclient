ee97f776e374b811c6370e6bd93371e2295cf4ed
darek-giza <darek_giza@op.pl>
2020-04-13 19:45:52 +0200

    Implements fetch data from api to pie chart

--- src/components/Charts/ChartPie.js
+++ src/components/Charts/ChartPie.js
@@ -1,19 +1,64 @@
-import React, { useCallback } from 'react';
-import { Container } from '@material-ui/core';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Chart.css';
-import AsyncPie from "./AsyncPie";
+import PieCharts from "./PieCharts";
 import { request } from "../../utils/request";
-
+import { Button, Container } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+import Paper from "@material-ui/core/Paper";
+import Spinner from "../Spinner";
 
 const ChartPie = () => {
+  const [data, setDat] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-  const load = useCallback(async () => {
-    return await request('/api/chartPie');
+  const fetch = useCallback(async () => {
+    setLoading(true);
+    setError('');
+    try {
+      const data = await request('/api/chartPie');
+      console.log("DATA",data)
+      setDat(data);
+    } catch {
+      setError("Couldn't load chart data");
+    } finally {
+      setLoading(false);
+    }
   }, []);
+
+  useEffect(() => {
+    fetch();
+  },[]);
+
+
+
+  //   if (shouldRefresh) {
+  //     fetch();
+  //     onRefresh();
+  //   }
+  // }, [shouldRefresh]);
+
+  const refresh = useCallback(() => {
+    setError('');
+    fetch();
+  }, []);
+
+  if (error) {
+    return (
+      <Button onClick={refresh} className="type-button">
+        <Alert severity="error" variant="filled">
+          {error}
+        </Alert>
+      </Button>
+    );
+  }
 
   return (
     <Container>
-      <AsyncPie/>
+      <Paper className="chart-container">
+        {isLoading && <Spinner/>}
+        <PieCharts data={data}/>
+      </Paper>
     </Container>
   );
 };
